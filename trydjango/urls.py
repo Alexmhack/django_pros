12cc933692fdd9a581f82ed15e9748206a227eba
alexHooper <alexmohack@gmail.com>
2018-08-03 21:37:11 +0530

    make some slight changes to correct error

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
