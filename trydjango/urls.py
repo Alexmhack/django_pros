5b1a57ebd7405ca27e6f321fea8afe84325011f1
alexHooper <alexmohack@gmail.com>
2018-08-03 21:35:14 +0530

    add root url to home_view

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import home_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
 ]
