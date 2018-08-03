e3271da985604234db3281249a623a2251594eb9
alexHooper <alexmohack@gmail.com>
2018-08-03 21:39:01 +0530

    import the whole view from pages

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages import views
+from pages.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
+    path('', home_view, name='home'),
 ]
