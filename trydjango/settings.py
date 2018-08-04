58091fcff227efc59f4db166af3626371ec06495
alexHooper <alexmohack@gmail.com>
2018-08-05 00:05:30 +0530

    change order of defining variables

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -125,10 +125,11 @@ STATIC_URL = '/static/'
 # EMAIL BACKEND
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'thisisauselessemail'
-EMAIL_HOST_USER = "78030psg@gmail.com"
+EMAIL_HOST_USER = '78030psg@gmail.com'
+EMAIL_HOST_PASSWORD = "thisisauselessemail"
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_SUBJECT_PREFIX = "Django-Webapp"
 EMAIL_USE_LOCALTIME = True
-EMAIL_USE_TLS = True
