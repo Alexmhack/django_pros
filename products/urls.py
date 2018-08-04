ee7d14016c5f89626a4e53843c88b5c4ed239ec7
alexHooper <alexmohack@gmail.com>
2018-08-05 00:04:26 +0530

    add url for contact confirm

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 
 from .views import (
 	home_view,
-	contact_view)
+	contact_view,
+	contact_confirm_view)
 
 urlpatterns = [
 	path('', home_view, name='home'),
 	path('contact/', contact_view, name='contact'),
+	path('contact/confirm/', contact_confirm_view, name='contact_confirm')
 ]
