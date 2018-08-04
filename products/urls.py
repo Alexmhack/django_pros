f7af5001b9883198c0eb1ecde136e277d0e61e00
alexHooper <alexmohack@gmail.com>
2018-08-04 23:40:07 +0530

    import our views from views.py

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_view
+from .views import (
+	home_view,
+	contact_view)
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('contact/', contact_view, name='contact'),
+	path('', home_view, name='home'),
+	path('contact/', contact_view, name='contact'),
 ]
