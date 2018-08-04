2c0f470ff8a2898047cba57eb95a4285df50f2ce
alexHooper <alexmohack@gmail.com>
2018-08-04 23:09:37 +0530

    file for our app urls

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import home_view
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('contact/', contact_view, name='contact'),
+]
