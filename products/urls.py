716763fef7b1632057f17508e32664216af56942
alexHooper <alexmohack@gmail.com>
2018-08-05 00:09:42 +0530

    delete view import and view url

--- products/urls.py
+++ products/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 
 from .views import (
 	home_view,
-	contact_view,
-	contact_confirm_view)
+	contact_view,)
 
 urlpatterns = [
 	path('', home_view, name='home'),
 	path('contact/', contact_view, name='contact'),
-	path('contact/confirm/', contact_confirm_view, name='contact_confirm')
 ]
