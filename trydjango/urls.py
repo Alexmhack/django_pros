ffec04be311deeb9365cd68f48f68356801b3e80
alexHooper <alexmohack@gmail.com>
2018-08-17 10:50:51 +0530

    correct error by importing our view

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from pages.views import home_view
+from pages.views import home_view, contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
