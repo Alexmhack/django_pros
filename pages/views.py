39e1f2b14907ed7272e31542ada2b4003fd3a0b4
alexHooper <alexmohack@gmail.com>
2018-08-17 10:55:17 +0530

    use request for activating our view

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
 	return HttpResponse("<h2>HOME PAGE VIEW</h2>")
 
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
 	return HttpResponse("<h2>CONTACT VIEW</h2>")
