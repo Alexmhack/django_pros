98ec449ddbb3bd489b72cf86f4d36fafcb1d1940
alexHooper <alexmohack@gmail.com>
2018-08-17 11:05:44 +0530

    import our view and create its url

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from pages.views import home_view, contact_view
+from pages.views import (
+    home_view,
+    contact_view,
+    about_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
+    path('about/', about_view, name='about'),
 ]
