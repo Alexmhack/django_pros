7c555f043b4a3442f6eac1525f70bfc80e5aae3d
alexHooper <alexmohack@gmail.com>
2018-08-17 10:51:33 +0530

    add contact page view

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home_view(*args, **kwargs):
 	return HttpResponse("<h2>HOME PAGE VIEW</h2>")
+
+
+def contact_view(*args, **kwargs):
+	return HttpResponse("<h2>CONTACT VIEW</h2>")
