10e99d878748733074f70b70a51dd134ee07526f
alexHooper <alexmohack@gmail.com>
2018-08-17 11:05:02 +0530

    use render and create new view for about page

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
-	return HttpResponse("<h2>HOME PAGE VIEW</h2>")
+	return render(request, 'home.html', {})
 
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h2>CONTACT VIEW</h2>")
+	return render(request, 'contact.html', {})
+
+
+def about_view(request):
+	return render(request, 'about.html', {})
