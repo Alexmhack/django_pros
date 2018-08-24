dd97caf54b54681cafb59fc2071939430f99cc62
alexHooper <alexmohack@gmail.com>
2018-08-24 09:00:11 +0530

    add default False to featured field

--- products/models.py
+++ products/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
 	description = models.TextField(blank=True, null=True)
 	price = models.DecimalField(decimal_places=2, max_digits=10000)
 	summary = models.TextField(blank=False, null=False)
-	featured = models.BooleanField()
+	featured = models.BooleanField(default=False)
