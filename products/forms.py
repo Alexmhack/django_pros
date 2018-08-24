db843bdbc655d856c77a4de04d5da60c9575cf9d
alexHooper <alexmohack@gmail.com>
2018-08-24 10:27:24 +0530

    use advance form fields features and attributes

--- products/forms.py
+++ products/forms.py
@@ -13,6 +13,24 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-	title = forms.CharField(label='')
-	description = forms.CharField(required=False, widget=forms.Textarea)
+	title = forms.CharField(
+		label='',
+		widget=forms.TextInput(
+			attrs={
+				"placeholder": "Your Title",
+				"class": "form-control"
+			}
+		)
+	)
+	description = forms.CharField(
+		required=False,
+		widget=forms.Textarea(
+			attrs={
+				"class": "form-control",
+				"rows": 4,
+				"cols": 50,
+				'placeholder': "Your Description"
+			}
+		)
+	)
 	price = forms.DecimalField(initial=299.99)
