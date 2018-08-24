988b736fbe965f69205a56e30ad977d52148b48b
alexHooper <alexmohack@gmail.com>
2018-08-24 10:01:27 +0530

    use raw form in views and create object with cleaned data

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
 
 def product_detail_view(request):
 	obj = Product.objects.all()
@@ -12,10 +12,14 @@ def product_detail_view(request):
 
 
 def product_create_view(request):
-	form = ProductForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-
+	form = RawProductForm()
+	if request.method == "POST":
+		form = RawProductForm(request.POST)
+		if form.is_valid():
+			print(form.cleaned_data)
+			Product.objects.create(**form.cleaned_data)
+		else:
+			print(form.errors)
 	context = {
 		'form': form
 	}
